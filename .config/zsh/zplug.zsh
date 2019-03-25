ccc6742d4925e145631747e2eaf8ab8b0b62da46
Benjamin Nørgaard <b@noergaard.dk>
2019-03-25 10:56:21 +0100

    load ssh-agent plugin

--- .config/zsh/zplug.zsh
+++ .config/zsh/zplug.zsh
@@ -6,6 +6,7 @@ zplug "denysdovhan/spaceship-prompt", use:spaceship.zsh, from:github, as:theme
 zplug "zsh-users/zsh-autosuggestions"
 zplug "zsh-users/zsh-history-substring-search"
 zplug "plugins/kubectl", from:oh-my-zsh
+zplug "plugins/ssh-agent", from:oh-my-zsh
 zplug "zsh-users/zsh-completions"
 zplug "lib/termsupport", from:oh-my-zsh
 
