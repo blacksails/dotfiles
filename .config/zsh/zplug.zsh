ac503b8caf525dac0eed16a82430d320f8430ae4
Benjamin Nørgaard <b@noergaard.dk>
2019-09-09 13:53:13 +0200

    reorganize zplug and add completion file from omzsh

--- .config/zsh/zplug.zsh
+++ .config/zsh/zplug.zsh
@@ -3,11 +3,15 @@ export ZPLUG_HOME=$XDG_DATA_HOME/zplug
 source $ZPLUG_HOME/init.zsh
 
 zplug "denysdovhan/spaceship-prompt", use:spaceship.zsh, from:github, as:theme
-zplug "zsh-users/zsh-autosuggestions"
-zplug "zsh-users/zsh-history-substring-search"
+
+zplug "lib/completion", from:oh-my-zsh
+zplug "lib/termsupport", from:oh-my-zsh
 zplug "plugins/kubectl", from:oh-my-zsh
 zplug "plugins/ssh-agent", from:oh-my-zsh
+
+zplug "zsh-users/zsh-autosuggestions"
+zplug "zsh-users/zsh-history-substring-search"
 zplug "zsh-users/zsh-completions"
-zplug "lib/termsupport", from:oh-my-zsh
+zplug "docker/cli", as:plugin, use:"contrib/completion/zsh/_docker"
 
 zplug load
