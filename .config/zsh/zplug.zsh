9ddb02229e79b7f4ac8acf3378d50010ac01cc8e
Benjamin Nørgaard <b@noergaard.dk>
2020-11-20 09:09:42 +0100

    remove ssh and gpg agent plugins

--- .config/zsh/zplug.zsh
+++ .config/zsh/zplug.zsh
@@ -7,8 +7,6 @@ zplug "denysdovhan/spaceship-prompt", use:spaceship.zsh, from:github, as:theme
 zplug "lib/completion", from:oh-my-zsh
 zplug "lib/termsupport", from:oh-my-zsh
 zplug "plugins/kubectl", from:oh-my-zsh
-zplug "plugins/ssh-agent", from:oh-my-zsh
-zplug "plugins/gpg-agent", from:oh-my-zsh
 zplug "plugins/asdf", from:oh-my-zsh
 zplug "plugins/docker", from:oh-my-zsh
 zplug "plugins/git", from:oh-my-zsh
