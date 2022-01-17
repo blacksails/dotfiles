6209ebc195233b39dcdcc7b006a3cce1642b06b7
Benjamin Nørgaard <b@noergaard.dk>
2022-01-17 11:18:27 +0100

    update mac support

--- .config/zsh/conf.d/asdf.zsh
+++ .config/zsh/conf.d/asdf.zsh
@@ -1,3 +1,3 @@
 export ASDF_HOME=$XDG_DATA_HOME/asdf
 source $ASDF_HOME/asdf.sh
-source $ASDF_HOME/completions/asdf.bash
+#source $ASDF_HOME/completions/asdf.bash
--- .config/zsh/conf.d/npm.zsh
+++
@@ -1,1 +0,0 @@
-export NODE_EXTRA_CA_CERTS="$HOME/Documents/certificates/tradeshift_ca.cert"
