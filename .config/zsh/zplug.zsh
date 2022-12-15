381a28ab6b9c5ea69581b5198b7273d147c88db2
Benjamin Nørgaard <mail@blacksails.dev>
2022-12-15 09:30:36 +0100

    Replace spaceship with starship

--- .config/zsh/zplug.zsh
+++ .config/zsh/zplug.zsh
@@ -2,8 +2,6 @@ export ZPLUG_HOME=$XDG_DATA_HOME/zplug
 
 source $ZPLUG_HOME/init.zsh
 
-zplug "denysdovhan/spaceship-prompt", use:spaceship.zsh, from:github, as:theme
-
 zplug "lib/completion", from:oh-my-zsh
 zplug "lib/termsupport", from:oh-my-zsh
 zplug "plugins/kubectl", from:oh-my-zsh
