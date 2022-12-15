3a6b817df2e366d6118e2cad70adb35904627609
Benjamin Nørgaard <mail@blacksails.dev>
2022-12-15 09:31:56 +0100

    Correct asdf config dir

--- .config/zsh/conf.d/asdf.zsh
+++ .config/zsh/conf.d/asdf.zsh
@@ -1,4 +1,4 @@
-export ASDF_CONFIG_DIR="${XDG_DATA_HOME}/asdf"
+export ASDF_CONFIG_DIR="${XDG_CONFIG_HOME}/asdf"
 export ASDF_CONFIG_FILE="${ASDF_CONFIG_DIR}/asdfrc"
 export ASDF_DIR="${XDG_DATA_HOME}/asdf"
 export ASDF_DATA_DIR="${ASDF_DIR}"
