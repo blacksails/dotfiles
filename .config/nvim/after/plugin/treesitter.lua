febc26fa63c5ee0ea9acf17d091e4862e81768b5
Benjamin Nørgaard <mail@blacksails.dev>
2023-05-22 12:34:28 +0200

    Add tree-sitter support for go templates

--- .config/nvim/after/plugin/treesitter.lua
+++ .config/nvim/after/plugin/treesitter.lua
@@ -1,8 +1,23 @@
 require('nvim-treesitter.configs').setup {
-  ensure_installed = { 'lua', 'go', 'gomod', 'gosum' },
+  ensure_installed = { 'lua', 'go', 'gomod', 'gosum', 'yaml' },
   auto_install = true,
   highlight = {
     enable = true,
     additional_vim_regex_highlighting = false,
   },
 }
+
+local parser_config = require('nvim-treesitter.parsers').get_parser_configs()
+
+parser_config.gotmpl = {
+  install_info = {
+    url = "https://github.com/ngalaiko/tree-sitter-go-template",
+    files = {"src/parser.c"},
+  },
+  filetype = "gotmpl",
+  used_by = {"gohtmltmpl", "gotexttmpl", "gotmpl", "yaml"}
+}
+
+local queries = require('vim.treesitter.query')
+
+queries.set("gotmpl", "injections", '(text) @yaml @combined')
