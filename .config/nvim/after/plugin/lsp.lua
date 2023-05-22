34fb1085fe599b810ddfa736b24873d0f59c6efe
Benjamin Nørgaard <mail@blacksails.dev>
2023-05-22 12:29:10 +0200

    add lua lsp

--- .config/nvim/after/plugin/lsp.lua
+++ .config/nvim/after/plugin/lsp.lua
@@ -5,12 +5,12 @@ lsp.preset("recommended")
 lsp.ensure_installed({
   'tsserver',
   'rust_analyzer',
-  'gopls'
+  'gopls',
+  'lua_ls',
 })
 
 -- Fix Undefined global 'vim'
 lsp.nvim_workspace()
-
 
 local cmp = require('cmp')
 local cmp_select = {behavior = cmp.SelectBehavior.Select}
