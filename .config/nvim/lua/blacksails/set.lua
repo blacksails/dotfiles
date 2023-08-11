8840501d1b0afd73e00e7957327b9f2c7fd073ab
Benjamin Nørgaard <mail@blacksails.dev>
2023-08-11 08:54:08 +0200

    update vim options

--- .config/nvim/lua/blacksails/set.lua
+++ .config/nvim/lua/blacksails/set.lua
@@ -6,11 +6,11 @@ vim.opt.relativenumber = true
 vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.softtabstop = 4
-vim.opt.expandtab = false
+vim.opt.expandtab = true
 
 vim.opt.smartindent = true
 
-vim.opt.wrap = false
+vim.opt.wrap = true
 
 vim.opt.swapfile = false
 vim.opt.backup = false
@@ -27,3 +27,6 @@ vim.opt.isfname:append("@-@")
 vim.opt.updatetime = 50
 
 vim.opt.colorcolumn = "80"
+
+vim.opt.splitbelow = true
+vim.opt.splitright = true
