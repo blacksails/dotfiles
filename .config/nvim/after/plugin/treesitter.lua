657844e202f871b186c46baabba0028b64fc35de
Benjamin Nørgaard <mail@blacksails.dev>
2023-08-11 08:55:29 +0200

    enable treesitter indentation

--- .config/nvim/after/plugin/treesitter.lua
+++ .config/nvim/after/plugin/treesitter.lua
@@ -5,6 +5,9 @@ require('nvim-treesitter.configs').setup {
     enable = true,
     additional_vim_regex_highlighting = false,
   },
+  indent = {
+      enable = true
+  }
 }
 
 local parser_config = require('nvim-treesitter.parsers').get_parser_configs()
