bcee41a934755a943b82755ecfddf789c5915bbd
Benjamin Nørgaard <mail@blacksails.dev>
2023-08-11 08:53:02 +0200

    remove lib/termsupport from oh-my-zsh

--- .config/zsh/zplug.zsh
+++ .config/zsh/zplug.zsh
@@ -3,7 +3,6 @@ export ZPLUG_HOME=$XDG_DATA_HOME/zplug
 source $ZPLUG_HOME/init.zsh
 
 zplug "lib/completion", from:oh-my-zsh
-zplug "lib/termsupport", from:oh-my-zsh
 zplug "plugins/kubectl", from:oh-my-zsh
 zplug "plugins/asdf", from:oh-my-zsh
 zplug "plugins/docker", from:oh-my-zsh
