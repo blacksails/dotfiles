14f2d260022953e37333036d797867d780ae7555
Benjamin Nørgaard <mail@blacksails.dev>
2024-05-03 09:20:22 +0200

    Add java config

--- .config/zsh/conf.d/asdf.zsh
+++ .config/zsh/conf.d/asdf.zsh
@@ -3,3 +3,8 @@ export ASDF_CONFIG_FILE="${ASDF_CONFIG_DIR}/asdfrc"
 export ASDF_DIR="${XDG_DATA_HOME}/asdf"
 export ASDF_DATA_DIR="${ASDF_DIR}"
 source $ASDF_DIR/asdf.sh
+
+# Setup java if plugin is enabled
+if [ -f $ASDF_DIR/plugins/java/set-java-home.zsh ]; then
+  . $ASDF_DIR/plugins/java/set-java-home.zsh
+fi
