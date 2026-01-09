08a845a885605f570dcc93b4688bdec387f84531
Benjamin Nørgaard <mail@blacksails.dev>
2026-01-09 21:49:38 +0100

    change dotfiles lazygit alias

--- .config/zsh/conf.d/dotfiles.zsh
+++ .config/zsh/conf.d/dotfiles.zsh
@@ -1,4 +1,4 @@
 alias dotf='git --git-dir ~/.files/ --work-tree ~'
 if command -v lazygit >/dev/null 2>&1; then
-  alias dotflg='lazygit --git-dir ~/.files/ --work-tree ~'
+  alias dotfl='lazygit --git-dir ~/.files/ --work-tree ~'
 fi
